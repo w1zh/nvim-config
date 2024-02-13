bbc2d9acf5e891fcd28b443ac86515b3871673b6
w1zh <generalwizh@live.se>
2024-02-13 18:26:03 +0100

    added toggleterm plugin

--- nvim/lua/w1zh/remap.lua
+++ nvim/lua/w1zh/remap.lua
@@ -36,3 +36,5 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>i", "<cmd>ToggleTerm size=10 dir=~/Documents/Programming/ direction=horizontal<CR>")
